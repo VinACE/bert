9bc8b236af7b5fa411d9a9e807f6bfca73ee2eb1
vinace <vinsent.storage@gmail.com>
2019-05-07 19:11:32 +0530

    testing the model download and path changes

--- pytorch_pretrained_bert/file_utils.py
+++ pytorch_pretrained_bert/file_utils.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 # PYTORCH_PRETRAINED_BERT_CACHE = Path(os.getenv('PYTORCH_PRETRAINED_BERT_CACHE',  '/demo-mount/.pytorch_pretrained_bert/'))
 
-PYTORCH_PRETRAINED_BERT_CACHE = '/demo-mount/.pytorch_pretrained_bert/'
+PYTORCH_PRETRAINED_BERT_CACHE = Path(os.environ['PYTORCH_PRETRAINED_BERT_CACHE'])
 
 def url_to_filename(url: str, etag: str = None) -> str:
     """
